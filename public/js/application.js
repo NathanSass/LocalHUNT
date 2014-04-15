69339b1964f3a426e225d186bdf42b38d0816e17
Apprentice <apprentice@dbc23.local>
2014-04-15 16:55:36 -0700

    test.erb shows something

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,16 @@
+
+
 $(document).ready(function() {
   // This is called after the document has loaded in its entirety
+  var mapOptions = {
+      center: new google.maps.LatLng(37.7831,-122.4039),
+      zoom: 12,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  new google.maps.Map(document.getElementById('map'), mapOptions);
+  console.log('js is loaded')
+
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
 
