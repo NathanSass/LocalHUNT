7b018c6bf1ab5ebf004d4a121581b79d3b4f73f3
Apprentice <apprentice@dbc23.local>
2014-04-15 17:02:05 -0700

    map displays in a specified pixel range TODO add a click

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,7 @@
 
 
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
+
   var mapOptions = {
       center: new google.maps.LatLng(37.7831,-122.4039),
       zoom: 12,
@@ -9,10 +9,5 @@ $(document).ready(function() {
   };
 
   new google.maps.Map(document.getElementById('map'), mapOptions);
-  console.log('js is loaded')
 
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });
