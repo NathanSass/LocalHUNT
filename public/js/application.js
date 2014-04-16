219fd837c72ab0b596e023ebbc78eed4fd4efd0d
Apprentice <apprentice@dbc23.local>
2014-04-15 18:37:27 -0700

    able to click on map to add a point, TODO add description to dropped pins

--- public/js/application.js
+++ public/js/application.js
@@ -1,13 +1,81 @@
 
-
 $(document).ready(function() {
 
   var mapOptions = {
-      center: new google.maps.LatLng(37.7831,-122.4039),
+      center: new google.maps.LatLng(37.7831, -122.4039),
       zoom: 12,
       mapTypeId: google.maps.MapTypeId.ROADMAP
   };
+  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-  new google.maps.Map(document.getElementById('map'), mapOptions);
+  var markerOptions = {
+      position: new google.maps.LatLng(37.7831, -122.4039)
+  };
+  var marker = new google.maps.Marker(markerOptions);
+  marker.setMap(map);
 
-});
+  google.maps.event.addListener(map, 'click', function(event) {
+   placeMarker(event.latLng);
+  });
+
+  function placeMarker(location) {
+    var marker = new google.maps.Marker({
+      position: location,
+      map: map
+    });
+  }
+
+})
+
+///below
+
+// function GoogleAPI(){
+//   this.map; //very important semi-colon
+//   this.mapOptions = {
+//       center: new google.maps.LatLng(37.7831, -122.4039),
+//       zoom: 12,
+//       mapTypeId: google.maps.MapTypeId.TERRAIN
+//   };
+// }
+
+// GoogleAPI.prototype = {
+//   initialize: function(){
+//    this.map = new google.maps.Map(document.getElementById('map'), this.mapOptions);
+//   },
+// }
+
+// markerOptions = {
+
+// }
+
+// function Marker(lat, longi){
+//   this.LatLng = LatLng(lat, longi)
+//   this.placeMarker =
+
+// }
+
+// Marker.prototype = {
+//   createMarker: function(markerOptions){
+//     return new google.maps.Marker(markerOptions)
+//   },
+//   setMarker: function(createdMarker){
+//     createdMarker.setMap(this.map)
+//   }
+// }
+
+
+// $(document).ready(function() {
+
+
+// var myMap = new GoogleAPI()
+// myMap.initialize()
+
+// var markerOptions = {
+//     position: new google.maps.LatLng(37.7831, -122.4039)
+// };
+
+
+// var myMarker = myMap.createMarker(markerOptions)
+// myMarker.setMap(myMap);
+// });
+
