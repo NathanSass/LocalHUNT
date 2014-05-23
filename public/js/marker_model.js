7eef190f9ded8c6833098d259f2abdd96ef57322
Nathan Sass <nsass711@gmail.com>
2014-05-23 14:15:49 -0700

    able to print initial coords and after coords TODO update marker click listeners and prepare to be able to update locations

--- public/js/marker_model.js
+++ public/js/marker_model.js
@@ -9,10 +9,19 @@ Marker.prototype = {
         position:  location,
         map:       this.map,
         clickable: true,
-        draggable: true //find a way to toggle this
+        draggable: true,
+        icon:      '../img/green_icon.png'
       });
       this.addLabel(marker)
       this.bindShowContentListener(marker)
+      
+      google.maps.event.addListener(marker, 'dragstart', function() {
+        console.log(marker.getPosition());
+      });
+      google.maps.event.addListener(marker, 'dragend', function() {
+        console.log(marker.getPosition());
+      });
+
       var markerObject = this.prepareMarkerForAjax(marker)
       /////////////////
       ///AJAX CALL HERE
@@ -26,6 +35,10 @@ Marker.prototype = {
     });
   },
 
+  bindMarkerListeners: function(){
+    this.bindShowContentListener();
+  }
+
   bindShowContentListener: function(marker){
     google.maps.event.addListener(marker, 'click', function() {
       marker.info.open(this.map, marker);
@@ -33,11 +46,15 @@ Marker.prototype = {
   },
 
   prepareMarkerForAjax: function(marker){
-    var lat       = marker["position"]["k"]
-    var longi     = marker["position"]["A"]
+    var lat       = this._roundNumber(marker["position"]["k"]);
+    var longi     = this._roundNumber(marker["position"]["A"]);
     var content   = marker.info["content"]
     var markerObj = { latitude: lat, longitude: longi, content: content }
     return markerObj
+  },
+
+  _roundNumber: function(num){
+    return Math.round(num * 1000) / 1000
   },
 
   ajaxSendtoDB: function(markerObj){
@@ -70,6 +87,7 @@ Marker.prototype = {
       this.placeDBMarker(latLng, content)
     }
   },
+
   placeDBMarker: function(location, content){
     var marker = new google.maps.Marker({
         animation:  google.maps.Animation.DROP,
@@ -81,6 +99,7 @@ Marker.prototype = {
       this.bindShowContentListener(marker)
       var markerObject = this.prepareMarkerForAjax(marker)
   },
+
   addDBLabel: function(marker, content){
     marker.info = new google.maps.InfoWindow({
       content: content
